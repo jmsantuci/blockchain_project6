dc05d88fdb22a2b4cfa51831c1d04e93897c4a6e
Jose Marcelo Santuci <jsantuci@MACCIT021195.local>
2019-08-07 20:17:49 -0300

    End of Project 6 -

---
+++ README.md
@@ -0,0 +1,26 @@
+# José Marcelo Santuci - Udacity Blockchain Course - Project 5
+
+This is the submission of Project 5 of Udacity Blockchain course.
+
+1. Contract Address: 0x94cC82D6B4E132f105A0BA2881A7c291f42ef830
+2. Libraries: I used chai and truffle-assertions to check events in unit tests and any-promise, bindings and truffle-assertions to deploy to rinkeby network
+3. Program version: 1.0.0
+4. Node version: v10.16.0
+5. Truffle version: v5.0.24 (core: 5.0.24)
+6. Web3 version: 2.1.2
+
+
+## UML
+
+### Class Diagram
+![UML Diagram](images/udacity-supply-chain-class.png)
+
+### Sequence Diagram
+![UML Diagram](images/udacity-supply-chain-sequence.png)
+
+### State Diagram
+![UML Diagram](images/udacity-supply-chain-state.png)
+
+### Activity Diagram
+![UML Diagram](images/udacity-supply-chain-activity.png)
+
---
+++ truffle.js
@@ -0,0 +1,37 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() {return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/2bce7c5f9bb24a6f8a9407f0609af9f4')},
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000,
+        skipDryRun: true
+    },
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.4.24",    // Fetch exact version from solc-bin (default: truffle's version)
+      docker: false,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+
+};
